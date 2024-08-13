82df71ea92c82cec75a34fcd9d18fc16ab82e4b9
Chen Liang <chen.liang.cn@outlook.com>
2024-08-13 15:25:26 +0800

    rough home page

--- src/landing/mod.rs
+++ src/landing/mod.rs
@@ -3,12 +3,46 @@ use leptos::*;
 /// Renders the home page of your application.
 #[component]
 pub fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <div class="contents">
+            <div class="header">
+                <table class="header-menu">
+                <tr>
+                    <td class="header">
+                        <img src="images/logo.png"/>
+                    </td>
+                    <td class="header_menu">
+                        <a href="/" class="header">首页</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">走进学校</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">课程中心</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">继续教育</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">师资力量</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">新闻中心</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">在线学习</a>
+                    </td>
+                    <td class="header_menu">
+                        <a href="#" class="header">就业招聘</a>
+                    </td>
+
+                    <td class="header_login"><a href="/admin" class="header">登陆</a></td>
+                    <td class="header_login"><a href="#" class="header">注册</a></td>
+                </tr>
+                </table>
+            </div>
+            <div></div>
+        </div>
     }
 }

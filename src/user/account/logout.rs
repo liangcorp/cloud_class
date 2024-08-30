6069053a9cf72a19d19d511d547f41461e98c207
Chen Liang <chen.liang.cn@outlook.com>
2024-08-30 11:04:35 +0800

    delete cache during logout

--- src/user/account/logout.rs
+++ src/user/account/logout.rs
@@ -2,7 +2,7 @@ use leptos::*;
 use server_fn::ServerFnError;
 
 #[server(Logout, "/api")]
-pub async fn user_logout() -> Result<(), ServerFnError> {
+pub async fn user_logout() -> Result<String, ServerFnError> {
     use leptos_axum::extract;
     use axum::http::header::{HeaderMap, HeaderValue};
     use crate::session::cache::CustomCache;
@@ -24,24 +24,27 @@ pub async fn user_logout() -> Result<(), ServerFnError> {
         None => "".to_string(),
     };
 
-    let session_token = cookie.split('=').nth(1).unwrap();
+    let session_token = cookie.split('=')
+        .nth(1)
+        .unwrap_or("");
+
     CustomCache::delete_cache(session_token)?;
 
-    //  改变网址到学生资料
+    // 改变网址到学生资料
     leptos_axum::redirect("/");
 
-    Ok(())
+    Ok("正在退出...".to_string())
 }
 
 #[component]
 pub fn LogoutPage() -> impl IntoView {
-    use crate::session::*;
 
     let (session_token, set_session_token) = create_signal("".to_string());
+
     view!{
         <Await
             // `future` provides the `Future` to be resolved
-            future=extract_session_token
+            future=user_logout
 
             // the data is bound to whatever variable name you provide
             let:session_token

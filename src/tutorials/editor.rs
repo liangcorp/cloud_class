48fd1df7faa6d341fd8264dc191929194045462a
Chen Liang <chen.liang.cn@outlook.com>
2024-09-12 17:35:25 +0800

    progress in web code editor

--- src/tutorials/editor.rs
+++ src/tutorials/editor.rs
@@ -1,30 +1,34 @@
 use leptos::*;
+use leptos::ev::KeyboardEvent;
 
 #[component]
 pub fn TutorialEditorArea() -> impl IntoView {
-    let (code, set_code) = create_signal("default".to_string());
+    let (code, set_code) = create_signal("<span style=\"color:red;\">temp default</span>".to_string());
 
-    let on_keyboard = move |ev: leptos::ev::KeyboardEvent| {
-        // stop the page from reloading!
-        // ev.prevent_default();
+    let on_keydown = move |ev: KeyboardEvent| {
 
-        if ev.key() == "click" || ev.code() == "0x000F" {
-            set_code.set(format!("{}    ", event_target_value(&ev)));
+        if ev.code() == "Tab" {
+            // stop the key action!
+            ev.prevent_default();
+            let space = KeyboardEvent::new("keydown");
+            space.set("Space");
+            // logging::log!("{:?}", space);
         }
-        logging::log!("keycode: {}", ev.key_code());
-        logging::log!("charcode: {}", ev.char_code());
+        logging::log!("keycode: {}", ev.code());
 
         // set_code.set(event_target_value(&ev));
     };
 
     view! {
-        <textarea
-            class="editor"
-            spellcheck="false"
-            prop:value=move || code.get()
-            on:keyboard=on_keyboard
-        >
-            { move || code.get() }
-        </textarea>
+        // <textarea
+        //     class="editor"
+        //     spellcheck="false"
+        //     prop:value=move || code.get()
+        //     on:keydown=on_keydown
+        // >
+        // </textarea>
+        <div class="editor" contenteditable="true" spellcheck="false" on:keydown=on_keydown>
+            <div inner_html={move || code.get()} />
+       </div>
     }
 }

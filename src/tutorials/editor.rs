a60f5a532d9fb3c0f4291ddaabce204954303ec5
Chen Liang <chen.liang.cn@outlook.com>
2024-09-13 15:19:31 +0800

    add feature that submits source and code print them out

--- src/tutorials/editor.rs
+++ src/tutorials/editor.rs
@@ -1,34 +1,32 @@
 use leptos::*;
 use leptos::ev::KeyboardEvent;
+// use web_sys::KeyboardEvent;
 
 #[component]
 pub fn TutorialEditorArea() -> impl IntoView {
-    let (code, set_code) = create_signal("<span style=\"color:red;\">temp default</span>".to_string());
+    let (code, set_code) = create_signal("".to_string());
 
     let on_keydown = move |ev: KeyboardEvent| {
 
         if ev.code() == "Tab" {
             // stop the key action!
             ev.prevent_default();
-            let space = KeyboardEvent::new("keydown");
-            space.set("Space");
-            // logging::log!("{:?}", space);
         }
-        logging::log!("keycode: {}", ev.code());
-
-        // set_code.set(event_target_value(&ev));
     };
 
     view! {
-        // <textarea
-        //     class="editor"
-        //     spellcheck="false"
-        //     prop:value=move || code.get()
-        //     on:keydown=on_keydown
-        // >
-        // </textarea>
-        <div class="editor" contenteditable="true" spellcheck="false" on:keydown=on_keydown>
-            <div inner_html={move || code.get()} />
-       </div>
+        <textarea
+            class="editor"
+            spellcheck="false"
+            prop:value=move || code.get()
+            on:keydown=on_keydown
+        >
+        </textarea>
+       //  <div class="text_area">
+       //      1<br/>2
+       //      <div class="editor" contenteditable="true" spellcheck="false" on:keydown=on_keydown>
+       //          <div inner_html={move || code.get()} />
+       //     </div>
+       // </div>
     }
 }
--- src/tutorials/mod.rs
+++ src/tutorials/mod.rs
@@ -1,34 +1,73 @@
-pub mod editor;
+// pub mod editor;
 pub mod output;
-pub mod console;
+// pub mod console;
 
 use leptos::*;
+use leptos::ev::KeyboardEvent;
 
-use editor::TutorialEditorArea;
+// use editor::TutorialEditorArea;
 use output::TutorialOutputArea;
-use console::TutorialConsoleArea;
+// use console::TutorialConsoleArea;
 
 #[component]
 pub fn TutorialPage() -> impl IntoView {
+    let (code, set_code) = create_signal("".to_string());
+
+    let input_element: NodeRef<html::Textarea> = create_node_ref();
+
+    let on_keydown = move |ev: KeyboardEvent| {
+
+        if ev.code() == "Tab" {
+            // stop the key action!
+            ev.prevent_default();
+        }
+    };
+
+    let on_submit = move |ev: leptos::ev::SubmitEvent| {
+        // stop the page from reloading!
+        ev.prevent_default();
+
+        // here, we'll extract the value from the input
+        let value = input_element
+            .get()
+            // event handlers can only fire after the view
+            // is mounted to the DOM, so the `NodeRef` will be `Some`
+            .expect("<input> should be mounted")
+            // `leptos::HtmlElement<html::Input>` implements `Deref`
+            // to a `web_sys::HtmlInputElement`.
+            // this means we can call`HtmlInputElement::value()`
+            // to get the current value of the input
+            .value();
+        set_code.set(value);
+    };
 
     view! {
         <div class="tutorial">
-            <div class="toolbar">
-                <button>保存</button>
-                <button>运行</button>
-            </div>
-            <div class="editor_area">
-                <div class="text_area">
-                    <TutorialEditorArea />
+            <form on:submit=on_submit>
+                <div class="toolbar">
+                    <input type="submit" value="运行" />
                 </div>
-                <div class="console_area">
-                    <TutorialConsoleArea />
+                <div class="editor_area">
+                    <div class="text_area">
+                        <textarea
+                            class="editor"
+                            spellcheck="false"
+                            prop:value=move || code.get()
+                            on:keydown=on_keydown
+                            node_ref=input_element
+                        >
+                        </textarea>
+                    </div>
+                    // <div class="console_area">
+                    //     <TutorialConsoleArea />
+                    // </div>
                 </div>
-            </div>
-            <div class="middlebar"></div>
+            </form>
             <div class="output_area">
+                { move || code.get() }
                 <TutorialOutputArea />
             </div>
+
         </div>
     }
 }

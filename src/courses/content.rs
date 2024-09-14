3902061ecc38f6e0951e1678762fbf43111993e7
Chen Liang <chen.liang.cn@outlook.com>
2024-09-14 15:30:58 +0800

    refactoring code to reduce tabs

--- src/courses/content.rs
+++ src/courses/content.rs
@@ -146,6 +146,33 @@ pub async fn get_chapter_content(chapter_id: String) -> Result<String, ServerFnE
 pub fn ContentPage() -> impl IntoView {
     use crate::session::*;
 
+    view! {
+        <Await
+            // `future` provides the `Future` to be resolved
+            future=extract_session_user
+
+            // the data is bound to whatever variable name you provide
+            let:session_user
+        >
+            {match session_user {
+                Ok(ok_username) => {
+                    match ok_username {
+                        Some(some_username) => {
+                            view! {
+                                <DisplayUserCourseContent username=some_username.to_string() />
+                            }
+                        }
+                        None => view! { <Redirect path="/courses" /> },
+                    }
+                }
+                Err(_) => view! { <Redirect path="/courses" /> },
+            }}
+        </Await>
+    }
+}
+
+#[component]
+pub fn DisplayUserCourseContent(username: String) -> impl IntoView {
     let (chapter_id, set_chapter_id) = create_signal("welcome-0000".to_string());
     let (show_chapters, set_show_chapters) = create_signal(Vec::new());
 
@@ -168,108 +195,79 @@ pub fn ContentPage() -> impl IntoView {
     );
 
     view! {
-        <Await
-            // `future` provides the `Future` to be resolved
-            future=extract_session_user
-
-            // the data is bound to whatever variable name you provide
-            let:session_user
-        >
-            {match session_user {
-                Ok(ok_username) => {
-                    match ok_username {
-                        Some(some_username) => {
-                            view! {
-                                {if course_id().is_some() {
-                                    spawn_local(async move {
-                                        match get_course_chapters(course_id().unwrap().clone())
-                                            .await
-                                        {
-                                            Ok(ok_course_chapters) => {
-                                                set_show_chapters.set(ok_course_chapters)
-                                            }
-                                            Err(_) => {
-                                                set_show_chapters.set(Vec::new());
-                                            }
-                                        }
-                                    })
-                                }}
-                                <div align="right" style="height:30px">
-                                    <table>
-                                        <tr>
-                                            <td style="padding-right:20px">
-                                                <a
-                                                    target="_blank"
-                                                    rel="noopener noreferrer"
-                                                    href=format!("/tutorials/{}", course_id().unwrap())
-                                                    class="tutorial_link"
-                                                >
-                                                    "⚒ 实验室"
-                                                </a>
-                                            </td>
-                                            <td class="header_login">
-                                                <a class="header" href="/courses">
-                                                    {some_username}
-                                                </a>
-                                            </td>
-                                            <td class="header_login">
-                                                <a href="/logout" class="home_login">
-                                                    "退出"
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </div>
-                                <div>
-                                    <div class="sidenav">
-                                        <ul style="list-style-type:none">
-                                            <For
-                                                each=move || show_chapters.get()
-                                                key=|state| (state.chapter_id.clone())
-                                                let:chapter
-                                            >
-                                                <li>
-                                                    <p>
-                                                        <a
-                                                            on:click=move |_| {
-                                                                set_chapter_id.set(chapter.chapter_id.clone());
-                                                            }
-                                                            href="#"
-                                                            class="chapter_selection"
-                                                        >
-                                                            <div
-                                                                style="float: left;"
-                                                                class:display=move || chapter.chapter_number == 0
-                                                            >
-                                                                <b style="padding-right:5px;">
-                                                                    {chapter.chapter_number}"."
-                                                                </b>
-                                                            </div>
-                                                            {chapter.title}
-                                                        </a>
-                                                    </p>
-                                                </li>
-                                            </For>
-                                        </ul>
-                                    </div>
-                                    <div class="main">
-                                        <Transition fallback=move || {
-                                            view! { <p>"正在下载课程章节..."</p> }
-                                        }>
-                                            <div inner_html=move || {
-                                                async_data.get().map(|value| value.unwrap().to_string())
-                                            } />
-                                        </Transition>
-                                    </div>
-                                </div>
-                            }
-                                .into_view()
-                        }
-                        None => view! { <Redirect path="/courses" /> }.into_view(),
+        {if course_id().is_some() {
+            spawn_local(async move {
+                match get_course_chapters(course_id().unwrap().clone()).await {
+                    Ok(ok_course_chapters) => set_show_chapters.set(ok_course_chapters),
+                    Err(_) => {
+                        set_show_chapters.set(Vec::new());
                     }
                 }
-                Err(_) => view! { <Redirect path="/courses" /> }.into_view(),
-            }}
-        </Await>
+            })
+        }}
+        <div align="right" style="height:30px">
+            <table>
+                <tr>
+                    <td style="padding-right:20px">
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href=format!("/tutorials/{}", course_id().unwrap())
+                            class="tutorial_link"
+                        >
+                            "⚒ 实验室"
+                        </a>
+                    </td>
+                    <td class="header_login">
+                        <a class="header" href="/courses">
+                            {username}
+                        </a>
+                    </td>
+                    <td class="header_login">
+                        <a href="/logout" class="home_login">
+                            "退出"
+                        </a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="sidenav">
+            <ul style="list-style-type:none">
+                <For
+                    each=move || show_chapters.get()
+                    key=|state| (state.chapter_id.clone())
+                    let:chapter
+                >
+                    <li>
+                        <p>
+                            <a
+                                on:click=move |_| {
+                                    set_chapter_id.set(chapter.chapter_id.clone());
+                                }
+                                href="#"
+                                class="chapter_selection"
+                            >
+                                <div
+                                    style="float: left;"
+                                    class:display=move || chapter.chapter_number == 0
+                                >
+                                    <b style="padding-right:5px;">{chapter.chapter_number}"."</b>
+                                </div>
+                                {chapter.title}
+                            </a>
+                        </p>
+                    </li>
+                </For>
+            </ul>
+        </div>
+        <div class="main">
+            <Transition fallback=move || {
+                view! { <p>"正在下载课程章节..."</p> }
+            }>
+                <div inner_html=move || {
+                    async_data.get().map(|value| value.unwrap().to_string())
+                } />
+            </Transition>
+        </div>
     }
 }

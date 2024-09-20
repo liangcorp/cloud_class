d57e736c0033efdaee3d7caa9b198ed500d1f804
Chen Liang <chen.liang.cn@outlook.com>
2024-09-20 17:20:07 +0800

    setup redis container

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+# setup directories
 mkdir student_codes
 
+# setup redis container
+podman pull redis
+podman create redis
+
+sudo mkdir -p  /var/redis/data
+sudo mkdir $PWD/redis-data
+sudo chmod 775 -R /var/redis/data
+sudo chmod 775 -R $PWD/redis-data
+
+podman run -d   --name redis_server   -v $PWD/redis-data:/var/redis/data    -p 6379:6379   redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=

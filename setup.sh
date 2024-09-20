2845938d883553b58ccff0d3cd1a7569e86cf12a
Chen Liang <chen.liang.cn@outlook.com>
2024-09-20 17:33:52 +0800

    add instruction for streamlit docker container

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # setup directories
 mkdir student_codes
 
+# install podman
+sudo dnf install podman
+
 # setup redis container
 podman pull redis
 podman create redis
@@ -13,3 +16,9 @@ sudo chmod 775 -R /var/redis/data
 sudo chmod 775 -R $PWD/redis-data
 
 podman run -d   --name redis_server   -v $PWD/redis-data:/var/redis/data    -p 6379:6379   redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=
+
+# setup streamlit docker for sample student -- student1
+cd docker
+podman build -t streamlit .
+podman run -p 8501:8501 -d --name student1 -it streamlit
+

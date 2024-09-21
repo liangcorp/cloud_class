52671327e90743647980706a023289b7bd98acde
Chen Liang <chen.liang.cn@outlook.com>
2024-09-21 10:43:19 +0800

    use docker as command over podman

--- setup.sh
+++ setup.sh
@@ -3,22 +3,22 @@
 # setup directories
 mkdir student_codes
 
-# install podman
-sudo dnf install podman
+# install docker
+sudo dnf install docker
 
 # setup redis container
-podman pull redis
-podman create redis
+docker pull redis
+docker create redis
 
 sudo mkdir -p  /var/redis/data
 sudo mkdir $PWD/redis-data
 sudo chmod 775 -R /var/redis/data
 sudo chmod 775 -R $PWD/redis-data
 
-podman run -d   --name redis_server   -v $PWD/redis-data:/var/redis/data    -p 6379:6379   redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=
+docker run -d   --name redis_server   -v $PWD/redis-data:/var/redis/data    -p 6379:6379   redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=
 
 # setup streamlit docker for sample student -- student1
 cd docker
-podman build -t streamlit .
-podman run -p 8501:8501 -d --name student1 -it streamlit
+docker build -t streamlit .
+docker run -p 8501:8501 -d --name student1 -it streamlit
 

efe199b1bf6383e2ab55ad2f12bfdc6329de779a
Chen Liang <chen.liang.cn@outlook.com>
2024-09-23 10:14:37 +0800

    refactoring

--- src/tutorials/execution.rs
+++ src/tutorials/execution.rs
@@ -10,24 +10,25 @@ pub async fn execute_user_code(code: String) -> Result<(), ServerFnError> {
         Ok(f) => f,
         Err(e) => {
             // logging::log!("ERROR <tutorials/execution.rs:9>: {}", e.to_string());
-            return Err(ServerFnError::Args(e.to_string()))
-        },
+            return Err(ServerFnError::Args(e.to_string()));
+        }
     };
 
     match file.write_all(&code.into_bytes()) {
         Ok(_) => {
-            match Command::new("podman")
+            match Command::new("docker")
                 .arg("cp")
                 .arg("student_codes/streamlist_student1.py")
                 .arg("student1:streamlit_app.py")
-                .output() {
-                    Ok(_output) => Ok(()),
-                    Err(e) => {
-                        // logging::log!("ERROR <tutorials/execution.rs:26>: {}", e.to_string());
-                        Err(ServerFnError::Args(e.to_string()))
-                    },
+                .output()
+            {
+                Ok(_output) => Ok(()),
+                Err(e) => {
+                    // logging::log!("ERROR <tutorials/execution.rs:26>: {}", e.to_string());
+                    Err(ServerFnError::Args(e.to_string()))
                 }
-        },
+            }
+        }
         Err(e) => Err(ServerFnError::Args(e.to_string())),
     }
 }

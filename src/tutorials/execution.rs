ae01d43c4259ce8912d084c6cb5476b33750a226
Chen Liang <chen.liang.cn@outlook.com>
2024-09-23 12:12:11 +0800

    use css over html styling

--- src/tutorials/execution.rs
+++ src/tutorials/execution.rs
@@ -5,10 +5,7 @@ pub fn TutorialExecutionArea() -> impl IntoView {
     view! {
         <div class="output_area">
             <Transition fallback=move || view! { <p>"正在联系..."</p> }>
-                <iframe
-                    style="width:100%;height:890px;border:none;"
-                    src="http://localhost:8501/"
-                ></iframe>
+                <iframe class="code_execution" src="http://localhost:8501/" />
             </Transition>
         </div>
     }

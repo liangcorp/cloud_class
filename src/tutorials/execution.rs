451903de9532d42fe77e0bfd834f4a8e91916f84
Chen Liang <chen.liang.cn@outlook.com>
2024-09-23 12:33:29 +0800

    remove junk code

--- src/tutorials/execution.rs
+++ src/tutorials/execution.rs
@@ -4,9 +4,7 @@ use leptos::*;
 pub fn TutorialExecutionArea() -> impl IntoView {
     view! {
         <div class="output_area">
-            <Transition fallback=move || view! { <p>"正在联系..."</p> }>
-                <iframe class="code_execution" src="http://localhost:8501/" />
-            </Transition>
+            <iframe class="code_execution" src="http://localhost:8501/" />
         </div>
     }
 }

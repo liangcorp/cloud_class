d4c8a60f29a313f5c95a5368abc65199c36cd1c0
Chen Liang <chen.liang.cn@outlook.com>
2024-09-24 08:46:15 +0800

    add registration field

--- src/student/account/register.rs
+++ src/student/account/register.rs
@@ -52,8 +52,34 @@ pub fn RegistrationPage() -> impl IntoView {
                                     class="login_form"
                                     style="width:100%"
                                     type="password"
-                                    // value=password
-                                    // node_ref=input_password
+                                    // value=cnfrm_password
+                                    // node_ref=input_cnfrm_password
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>"邮件地址"</td>
+                            <td style="padding-left:10px">
+                                <input
+                                    placeholder="请输入邮件地址"
+                                    class="login_form"
+                                    style="width:100%"
+                                    type="text"
+                                    // value=email
+                                    // node_ref=input_email
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>"手机号"</td>
+                            <td style="padding-left:10px">
+                                <input
+                                    placeholder="请输入手机号"
+                                    class="login_form"
+                                    style="width:100%"
+                                    type="text"
+                                    // value=mobile_no
+                                    // node_ref=input_mobile_no
                                 />
                             </td>
                         </tr>
@@ -63,27 +89,15 @@ pub fn RegistrationPage() -> impl IntoView {
                                 <input
                                     placeholder="请输入验证码"
                                     class="login_form"
-                                    // value=password
-                                    // node_ref=input_password
+                                    type="text"
+                                    // value=mobl_verify
+                                    // node_ref=input_mobl_verify
                                 />
-                                <button style="margin-left:10px;padding:10px">"获取验证码"</button>
+                                <button style="margin-left:10px;padding:10px;font-size:22;background:#333333;font-weight:8;">"获取验证码"</button>
                             </td>
                         </tr>
                         <tr>
-                            <td>"验证码"</td>
-                            <td style="padding-left:10px">
-                                <input
-                                    placeholder="请输入手机号"
-                                    class="login_form"
-                                    style="width:100%"
-                                    type="password"
-                                    // value=password
-                                    // node_ref=input_password
-                                />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><button>"注册"</button></td>
+                            <td><button style="padding:10px;font-size:22;background:#333333;font-weight:8;">"注册"</button></td>
                             <td style="padding-left:10px">
                                 <a href="/" class="header_menu">"返回主页"</a>
                             </td>

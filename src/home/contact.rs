6a3ef582c9e6d8b142361e7e5c8d65240d5ce065
Chen Liang <chen.liang.cn@outlook.com>
2024-09-30 17:26:07 +0800

    stylistic change for contact us

--- src/home/contact.rs
+++ src/home/contact.rs
@@ -13,7 +13,7 @@ pub fn ContactPage() -> impl IntoView {
             <table>
                 <tr>
                     <td class="content">
-                        <h1>"准备好带领您的团队应对改变了吗？"</h1>
+                        <h1>"准备好带领您的团队应对市场变化了吗？"</h1>
                         <h3>"让我们一起讨论如何为您提供帮助:"</h3>
                         <ul>
                             <li>"加速数字化转型"</li>
@@ -24,27 +24,27 @@ pub fn ContactPage() -> impl IntoView {
                     </td>
                     <td class="content" style="background-color:white">
                         <form>
-                            <table style="padding-top:30px;padding-left:50px;">
+                            <table style="padding-top:30px;padding-left:50px;border-spacing:5px;">
                                 <tr>
-                                    <td>"全名"</td>
+                                    <td>"全名:"</td>
                                     <td>
-                                        <input class="login-form" type="text" />
+                                        <input class="login-form" type="text" placeholder="请输入全名" />
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td>"邮件"</td>
+                                    <td>"邮件:"</td>
                                     <td>
-                                        <input class="login-form" type="text" />
+                                        <input class="login-form" type="text" placeholder="请输入邮件" />
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td>"电话号码"</td>
+                                    <td>"电话号码:"</td>
                                     <td>
-                                        <input class="login-form" type="text" />
+                                        <input class="login-form" type="text" placeholder="请输入电话号码" />
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td colspan="2">
+                                    <td colspan="2" style="padding-top:10px">
                                         <input class="submit-button" type="button" value="提交"/>
                                     </td>
                                 </tr>

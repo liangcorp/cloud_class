a6f33b959ba745411b3a164a9331d23997b0564c
Chen Liang <chen.liang.cn@outlook.com>
2024-09-30 23:24:53 +0800

    use proxy in china

--- setup.sh
+++ setup.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
-# setup directories
-mkdir student_codes
-
 # install podman
 sudo dnf install podman podman-docker
 
+sudo mkdir -p  /var/redis/data
+mkdir -p $HOME/containers/redis-data
+
+sudo chmod 775 -R /var/redis/data
+chmod 775 -R $HOME/containers/redis-data
+
 # setup redis container
-docker pull redis
+docker pull dockerproxy.cn/redis
 docker create redis
 
-sudo mkdir -p  /var/redis/data
-sudo mkdir $PWD/redis-data
-sudo chmod 775 -R /var/redis/data
-sudo chmod 775 -R $PWD/redis-data
-
-docker run -d   --name redis_server   -v $PWD/redis-data:/var/redis/data    -p 6379:6379   redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=
+docker run -d --name redis_server -v $HOME/containers/redis-data:/var/redis/data -p 6379:6379 redis --requirepass cikq5XxudvHKUzdPgbQWokCOOhfT8wGQKPsLhBx8Tlw=
 
 # setup streamlit docker for students
-cd docker
+cd containers/docker
 docker build -t streamlit .
 docker run -p 8501:8501 -d --name $1 -it streamlit
 
